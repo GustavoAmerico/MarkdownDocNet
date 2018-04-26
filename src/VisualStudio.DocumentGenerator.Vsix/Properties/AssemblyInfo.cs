ce639ecf3c5aedeb1b7952ff90d1c4ef69013fe2
GustavoAmerico <gustavo.americo@live.com>
2018-04-25 23:02:56 -0300

    Alterando informações de resources

--- src/VisualStudio.DocumentGenerator.Vsix/Properties/AssemblyInfo.cs
+++ src/VisualStudio.DocumentGenerator.Vsix/Properties/AssemblyInfo.cs
@@ -3,11 +3,11 @@ using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following set of attributes.
 // Change these attribute values to modify the information associated with an assembly.
-[assembly: AssemblyTitle("MarkdownVsix")]
+[assembly: AssemblyTitle("Document Generator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MarkdownVsix")]
+[assembly: AssemblyProduct("Document Generator")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
